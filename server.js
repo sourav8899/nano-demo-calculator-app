cec9d492f4a56f535783729125c7e1f8b81548a4
sourav <sourabsethi66@gmail.com>
2023-05-31 18:09:47 +0530

    updated code with body parser

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const bodyParser = require('body-parser');
+
 const express = require('express');
 const app = express();
-const ejs=require("ejs");
-const bodyparser=require("body-parser");
+
+const bodyParser=require("body-parser");
 const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const baseUrl = '/calculator'
 
