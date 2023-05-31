992e7d2b4cd0ce08c0e22c7b88f6152ec229a4e2
sourav <sourabsethi66@gmail.com>
2023-05-31 19:04:04 +0530

    bot

--- server.js
+++ server.js
@@ -3,33 +3,29 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator';
+const baseUrl = '/calculator'
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-
 baseRouter.get('/greeting', (req, res) => {
-  return res.status(200).send('Hello world!');
+    return res.status(200).send("Hello world!");
 });
 
 baseRouter.post('/add', (req, res) => {
-  var { num1, num2 } = req.body;
-
-
-  res.status(200).json({ "result":num1+num2 });
+    var {first,second} = req.body;
+    res.status(200).json({ "result":  first+second});
 });
+
 
 baseRouter.post('/subtract', (req, res) => {
-  var { num1, num2 } = req.body;
- 
-
-  res.json({ "result":num1-num2 });
+    var {first,second} = req.body;
+    res.json({ "result": first-second });
 });
 
-app.use(baseUrl, baseRouter);
 
+app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-  console.log('Server running at PORT', PORT);
+    console.log("Server running at PORT", PORT);
 });
