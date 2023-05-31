34b9eaf3c345471f579777a31ada9c359e55ddb4
sourav <sourabsethi66@gmail.com>
2023-05-31 17:45:48 +0530

    updated the html page

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-
+const ejs=require("ejs");
+const bodyparser=require("body-parser");
 const PORT = process.env.PORT || 8080;
 
 const baseUrl = '/calculator'
@@ -8,9 +10,26 @@ const baseUrl = '/calculator'
 app.use(express.json());
 
 const baseRouter = express.Router();
+app.set("view engine",'ejs');
 
+baseRouter.post('/addition',(req,res)=>{
+ res.send("hello");
+ res.redirect()
+})
+baseRouter.post('/sub',(req,res)=>{
+    return res.send("hello");
+    });
+baseRouter.post('/mul',(req,res)=>{
+        return res.send("hello");
+        });
+baseRouter.post('/div',(req,res)=>{
+            return res.send("hello");
+            });
+baseRouter.get('',(req,res)=>{
+    return res.render("home");
+});
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('hello');
 });
 
 baseRouter.post('/add', (req, res) => {
